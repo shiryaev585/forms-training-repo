6dc19b5823ded70873d6fa5b9ad2fdf076fe9e67
Alexander Shiryaev <alex_sh_v@mail.ru>
2021-05-20 18:07:25 +0300

    react-hook-form-lesson

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useForm } from 'react-hook-form';
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, submitCount },
+    watch,
+  } = useForm({mode: "onChange"});
+
+  const onSubmit = (data) => {
+    console.log('Send data: ', data);
+  };
+  const name = watch("name");
+  console.log("Name: " + name);
+
+  console.log(errors)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Form</h1>
+      <hr />
+      <input
+        defaultValue='Alexander'
+        {...register('name', { required: true, maxLength: 15 })}
+      />
+      {errors.name && <i style={{color: "red", marginLeft: "10px"}}>Required field, 15 symbols max</i>}
+      <hr />
+      <input {...register('age', {required: true, pattern: /\d?\d/, min: 18, max: 65})} />
+      {errors.age && <i style={{color: "red", marginLeft: "10px"}}>Required field; min age - 18, max age - 65</i>}
+      <hr />
+      <select {...register("answer")}>
+        <option value="yes">Yes</option>
+        <option value="no">No</option>
+      </select>
+      <hr/>
+      <input type='submit' />
+      <p>Send counter: {submitCount}</p>
+    </form>
   );
 }
 
